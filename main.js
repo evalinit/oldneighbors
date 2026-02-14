8737b4a43ec0952849f3d04bb7acd10d4bcc096e
evalinit <evalinit@gmail.com>
2026-02-13 21:44:36 -0700

    get back on track

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ const parseConfigFromQuery = () => {
 
     const includeSelf = asBool(qs.get('self'), false)
 
-    const radiusParam = qs.get('radius')
     const neighborsParam = qs.get('neighbors')
+    const radiusParam = qs.get('radius')
 
     let neighborRadius = 1
 
@@ -27,9 +27,7 @@ const parseConfigFromQuery = () => {
         neighborRadius = asInt(radiusParam, 1)
     } else if (neighborsParam !== null) {
         const neighbors = asInt(neighborsParam, 2)
-
-        if (neighbors % 2 === 0) neighborRadius = Math.max(1, neighbors / 2)
-        else neighborRadius = Math.max(1, neighbors)
+        neighborRadius = Math.max(1, Math.floor(neighbors / 2))
     }
 
     neighborRadius = clamp(neighborRadius, 1, 10)
@@ -37,7 +35,41 @@ const parseConfigFromQuery = () => {
     return { neighborRadius, includeSelf }
 }
 
-const computeDraftOrder = ({ neighborRadius, includeSelf }, priorStandings, currentStandings) => {
+const computeOriginalTwoNeighborScoreMap = (priorStandings, currentStandings) => {
+    const teams = {}
+
+    for (let i = 0; i < priorStandings.length; i++) {
+        const team = priorStandings[i]
+
+        let neighbor1, neighbor2
+
+        if (i === 0) {
+            neighbor1 = currentStandings.indexOf(priorStandings[1])
+            neighbor2 = currentStandings.indexOf(priorStandings[2])
+        } else if (i === priorStandings.length - 1) {
+            neighbor1 = currentStandings.indexOf(priorStandings[priorStandings.length - 3])
+            neighbor2 = currentStandings.indexOf(priorStandings[priorStandings.length - 2])
+        } else {
+            neighbor1 = currentStandings.indexOf(priorStandings[i - 1])
+            neighbor2 = currentStandings.indexOf(priorStandings[i + 1])
+        }
+
+        teams[team] = (neighbor1 + neighbor2) / 2
+    }
+
+    return teams
+}
+
+const computeDraftOrderOriginalTwoNeighborRule = (priorStandings, currentStandings) => {
+    const teams = computeOriginalTwoNeighborScoreMap(priorStandings, currentStandings)
+    return Object.keys(teams).sort((a, b) => teams[a] - teams[b])
+}
+
+const computeDraftOrderRadiusRule = ({ neighborRadius, includeSelf }, priorStandings, currentStandings) => {
+    if (neighborRadius === 1 && !includeSelf) {
+        return computeDraftOrderOriginalTwoNeighborRule(priorStandings, currentStandings)
+    }
+
     const teamsScore = {}
 
     for (let i = 0; i < priorStandings.length; i++) {
@@ -68,23 +100,13 @@ const computeDraftOrder = ({ neighborRadius, includeSelf }, priorStandings, curr
     return Object.keys(teamsScore).sort((a, b) => teamsScore[a] - teamsScore[b])
 }
 
-const buildText = ({ neighborRadius, includeSelf }, order) => {
-    const neighborsTotal = neighborRadius * 2
-    const selfText = includeSelf ? ' + self' : ''
-    const title = `Old Neighbors draft order (${neighborsTotal} neighbors${selfText})`
-
+const buildOrderText = (order) => {
     let out = ''
-    out += `${title}\n\n`
-
     let idx = 1
     for (const team of order) {
         out += `${idx}) ${team}\n`
         idx++
     }
-
-    out += `\n`
-    out += `The Wizards and the Jazz play each other two more times is all I'm saying.\n`
-
     return out
 }
 
@@ -128,7 +150,7 @@ const renderLinks = (urls) => {
     const notes = document.createElement('div')
     notes.innerText =
         '\nParams:\n' +
-        '  neighbors=2 means 1 on each side\n' +
+        '  neighbors=2 means 1 on each side (original rule)\n' +
         '  neighbors=4 means 2 on each side\n' +
         '  self=1 includes the team itself in the average\n'
     container.appendChild(notes)
@@ -149,8 +171,17 @@ const main = async () => {
     const currentStandings = data.current_standings ?? []
     const priorStandings = data.prior_standings ?? []
 
-    const order = computeDraftOrder(config, priorStandings, currentStandings)
-    const text = buildText(config, order)
+    const order = computeDraftOrderRadiusRule(config, priorStandings, currentStandings)
+
+    const neighborsTotal = config.neighborRadius * 2
+    const selfText = config.includeSelf ? ' + self' : ''
+    const title = `Old Neighbors draft order (${neighborsTotal} neighbors${selfText})`
+
+    let text = ''
+    text += `${title}\n\n`
+    text += buildOrderText(order)
+    text += `\n`
+    text += `The Wizards and the Jazz play each other two more times is all I'm saying.\n`
 
     document.body.innerText = text
 
